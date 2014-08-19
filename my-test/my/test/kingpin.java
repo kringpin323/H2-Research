cd6977a9d1ab40684820e3e9821d0c5fc4569bdb
kringpin323 <kringpin323@gmail.com>
2014-08-19 17:15:55 +0800

    test for client

--- my-test/my/test/kingpin.java
+++ my-test/my/test/kingpin.java
@@ -1,5 +1,28 @@
 package my.test;
 // 用来测试 git for windows 是否能用
-public class kingpin {
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class kingpin {
+	public static void main(String[] args) throws Exception{
+		Class.forName("org.h2.Driver");
+		
+		Connection conn = DriverManager.getConnection("jdbc:h2:tcp://10.11.1.13:9092/mydb", "sa", "");
+		Statement stmt = conn.createStatement();
+		
+		Statement stmt2 = conn.createStatement();
+		stmt2.executeUpdate("DROP TABLE IF EXISTS my_table");
+		stmt2.executeUpdate("CREATE TABLE IF NOT EXISTS my_table(name varchar(20))");
+		stmt2.executeUpdate("INSERT INTO my_table(name) VALUES('zhh')");
+
+		ResultSet rs = stmt2.executeQuery("SELECT name FROM my_table");
+		rs.next();
+		System.out.println(rs.getString(1));
+
+		stmt2.close();
+		conn.close();
+	}
 }
